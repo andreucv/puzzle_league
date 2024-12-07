b568e3a26392c90a85982a00fb277458406c0577
Andreu Cortes <andreucv@gmail.com>
2024-12-07 16:50:54 +0000

    Landing page and background color

--- puzzle_league_frontend/src/routes/+page.svelte
+++ puzzle_league_frontend/src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1 class="card">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    // import { t, locales, locale } from '$lib/translations';
+</script>
+
+<svelte:head>
+    <title>Puzzle League</title>
+</svelte:head>
+
+<div class="landing-page">
+    <h1 style="line-height: 1.5">Puzzle League</h1>
+    <p>
+        Puzzles League for everyone
+    </p>
+    <button class="btn bg-success-100" on:click={() => window.location.href = '/competitions_list'}>Explore Competitions</button>
+</div>
+
+<style>
+    .landing-page {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        padding: 2rem;
+    }
+
+    .landing-page h1 {
+        font-size: 2.5rem;
+        margin-bottom: 1rem;
+    }
+
+    .landing-page p {
+        font-size: 1.25rem;
+        margin-bottom: 2rem;
+        max-width: 600px;
+    }
+</style>

aa9d17dfb128cebea3b5172711f30c64d28bc9a8
Andreu Cortes <andreucv@gmail.com>
2024-12-07 16:15:21 +0000

    Configure HMR

--- puzzle_league_frontend/svelte.config.js
+++ puzzle_league_frontend/svelte.config.js
@@ -12,6 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	// To enable hot module reloading, we need to enable polling because of docker environment
+	server: {
+		watch: {
+			usePolling: true,
+		}
 	}
 };
 

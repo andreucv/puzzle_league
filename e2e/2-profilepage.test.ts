221dc36ccd482f67ebb846cfff9ee229a04213cd
andreucv <andreucv+github@gmail.com>
2025-06-13 18:56:54 +0200

    Add testing for Admin and Organizer

--- e2e/2-profilepage.test.ts
+++ e2e/2-profilepage.test.ts
@@ -13,3 +13,19 @@ test('GivenProfilePage_WhenSigningOut_ThenRedirectsToLoginPage', async ({ page }
     await page.getByRole('button', { name: 'Sign out' }).click();
     await page.waitForURL(/\/login.*/);
 });
+
+test('GivenProfilePage_WhenClickingOnHambugerMenu_ThenCreateCompetitionIsNotVisible', async ({ page }) => {
+    await page.goto('/profile');
+    await page.locator('#states-button').click();
+    await expect(page.getByRole('link', { name: 'Create Competition' }).first()).not.toBeVisible();
+});
+
+test.describe('GivenProfilePageOrganizer_WhenClickingOnHambugerMenu_ThenCreateCompetitionIsNotVisible', () => {
+    test.use({ storageState: "playwright/.auth/organizer_user.json" });
+
+    test('GivenProfilePageOrganizer_WhenClickingOnHambugerMenu_ThenCreateCompetitionIsNotVisible', async ({ page }) => {
+        await page.goto('/profile');
+        await page.locator('#states-button').click();
+        await expect(page.getByRole('link', { name: 'Create Competition' }).first()).toBeVisible();
+    });
+});
---
+++ e2e/auth-organizer.setup.ts
@@ -0,0 +1,27 @@
+import { test as setup, expect } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const authFile = path.join(__dirname, '../playwright/.auth/organizer_user.json');
+
+setup('authenticate_organizer', async ({ page }) => {
+    // Perform authentication steps. Replace these actions with your own.
+    await page.goto('/login');
+    await page.locator('#input_email').fill(process.env.TEST_ORGANIZER_USER_EMAIL);
+    await page.locator('#input_password').fill(process.env.TEST_ORGANIZER_USER_PASSWORD);
+    const loginButton = page.locator('#login_submit');
+    await loginButton.click();
+
+    // Wait until the page receives the cookies.
+    //
+    // Sometimes login flow sets cookies in the process of several redirects.
+    // Wait for the final URL to ensure that the cookies are actually set.
+    await page.waitForURL('/');
+    await page.goto('/profile');
+    await expect(page.getByText('Organizer', { exact: true }).first()).toBeVisible();
+    await page.goto('/');
+    await page.waitForURL('/');
+    await page.context().storageState({ path: authFile });
+});

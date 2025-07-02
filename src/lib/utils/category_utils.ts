5ceab715117e5001dc541d35d2a7fad9c8de6c20
andreucv <andreucv+github@gmail.com>
2025-07-02 21:46:21 +0200

    Category Type no wasm

--- src/lib/utils/category_utils.ts
+++ src/lib/utils/category_utils.ts
@@ -1,4 +1,4 @@
-import { CategoryType } from "@prisma/client/wasm";
+import { CategoryType } from "@prisma/client";
 
 export function getCategoryTypeName(type: CategoryType) {
     const typeNames: Record<CategoryType, string> = {
